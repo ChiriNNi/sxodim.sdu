7009fdac49861b21b73a2d2bdbf258edeba490de
ChiriNNi <bekarysbulat@gmail.com>
2025-05-04 04:50:46 +0500

    authorization with tokens

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework.routers import DefaultRouter
 from .views import UserViewSet, ClubViewSet, EventViewSet, EventRegistrationViewSet, NotificationViewSet
 
@@ -11,4 +12,5 @@ router.register(r'notifications', NotificationViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
--- sxodim/settings.py
+++ sxodim/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'rest_framework',
+    'rest_framework.authtoken',
     'django_filters',
     'psycopg2'
 ]
