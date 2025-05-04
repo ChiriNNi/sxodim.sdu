428f26e244f0d1e193219565044ec9310c24a4d3
ChiriNNi <bekarysbulat@gmail.com>
2025-05-04 08:03:25 +0500

    adasas

--- core/urls.py
+++ core/urls.py
@@ -2,8 +2,7 @@ from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework.routers import DefaultRouter
 from .views import UserViewSet, ClubViewSet, EventViewSet, EventRegistrationViewSet, NotificationViewSet
-from django.conf import settings
-from django.conf.urls.static import static
+
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
@@ -17,5 +16,3 @@ urlpatterns = [
     path('api-token-auth/', obtain_auth_token , name='api_token_auth'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
